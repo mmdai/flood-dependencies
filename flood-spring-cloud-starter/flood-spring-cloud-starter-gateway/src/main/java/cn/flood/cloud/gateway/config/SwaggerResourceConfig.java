d99c81616f88065a8790c3b006a7932ca610a87f
benming110 <daiming123.happy@163.com>
2021-09-25 13:07:56 +0800

    no message

--- flood-spring-cloud-starter/flood-spring-cloud-starter-gateway/src/main/java/cn/flood/cloud/gateway/config/SwaggerResourceConfig.java
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-gateway/src/main/java/cn/flood/cloud/gateway/config/SwaggerResourceConfig.java
@@ -1,20 +1,17 @@
 package cn.flood.cloud.gateway.config;
 
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.config.GatewayProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.support.NameUtils;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * swagger配置
@@ -24,59 +21,39 @@ import java.util.Set;
 @Slf4j
 @Component
 @Primary
-@AllArgsConstructor
 public class SwaggerResourceConfig implements SwaggerResourcesProvider {
 
-    /**
-     * swagger2默认的url后缀
-     */
-    private static final String SWAGGER2URL = "/v2/api-docs";
+    public static final String API_URI = "v2/api-docs";
 
-    /**
-     * 网关路由
-     */
     private final RouteLocator routeLocator;
+    private final GatewayProperties gatewayProperties;
 
-
-    /**
-     * 网关应用名称
-     */
-    @Value("${spring.application.name}")
-    private String self;
-
-    @Autowired
-    public SwaggerResourceConfig(RouteLocator routeLocator) {
+    public SwaggerResourceConfig(RouteLocator routeLocator, GatewayProperties gatewayProperties) {
         this.routeLocator = routeLocator;
+        this.gatewayProperties = gatewayProperties;
     }
-
-    private final static String EXCLUDE_MODULE = "flood-admin";
 
     @Override
     public List<SwaggerResource> get() {
         List<SwaggerResource> resources = new ArrayList<>();
-        List<String> routeHosts = new ArrayList<>();
-        // 由于我的网关采用的是负载均衡的方式，因此我需要拿到所有应用的serviceId
-        // 获取所有可用的host：serviceId
-        // 当前排除掉flood-admin模块的doc
-        routeLocator.getRoutes().filter(route -> route.getUri().getHost() != null)
-                .filter(route -> !self.equals(route.getUri().getHost()))
-                .filter(route -> !route.getUri().getHost().equalsIgnoreCase(EXCLUDE_MODULE))
-                .subscribe(route -> routeHosts.add(route.getUri().getHost()));
-
-        // 记录已经添加过的server，存在同一个应用注册了多个服务在nacos上
-        Set<String> repeated = new HashSet<>();
-        routeHosts.forEach(instance -> {
-            // 拼接url，样式为/serviceId/v2/api-info，当网关调用这个接口时，会自动通过负载均衡寻找对应的主机
-            String url = "/" + instance + SWAGGER2URL;
-            if (!repeated.contains(url)) {
-                repeated.add(url);
-                SwaggerResource swaggerResource = new SwaggerResource();
-                swaggerResource.setUrl(url);
-                swaggerResource.setName(instance);
-                swaggerResource.setSwaggerVersion("2.0.0");
-                resources.add(swaggerResource);
-            }
-        });
+        List<String> routes = new ArrayList<>();
+        //获取所有路由的ID
+        routeLocator.getRoutes().subscribe(route -> routes.add(route.getId()));
+        //过滤出配置文件中定义的路由->过滤出Path Route Predicate->根据路径拼接成api-docs路径->生成SwaggerResource
+        gatewayProperties.getRoutes().stream().filter(routeDefinition -> routes.contains(routeDefinition.getId())).forEach(route ->
+                route.getPredicates().stream()
+                        .filter(predicateDefinition -> ("Path").equalsIgnoreCase(predicateDefinition.getName()))
+                        .forEach(predicateDefinition -> resources.add(swaggerResource(route.getId(),
+                                predicateDefinition.getArgs().get(NameUtils.GENERATED_NAME_PREFIX + "0")
+                                        .replace("**", API_URI)))));
         return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;
     }
 }
