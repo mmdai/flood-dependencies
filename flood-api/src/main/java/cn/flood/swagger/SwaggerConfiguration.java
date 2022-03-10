314ff217d9a5465e2728fe3fa0bad22e618313b2
mmdai <daiming123.happy@163.com>
2022-03-10 17:47:51 +0800

    feat(module):   升级版本 springboot-2.6.4;                                             springcloud-2021.0.1

--- flood-api/src/main/java/cn/flood/swagger/SwaggerConfiguration.java
+++ flood-api/src/main/java/cn/flood/swagger/SwaggerConfiguration.java
@@ -1,21 +1,26 @@
 package cn.flood.swagger;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.core.env.Profiles;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -24,6 +29,7 @@ import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -100,5 +106,44 @@ public class SwaggerConfiguration {
 		return securityReferences;
 	}
 
+
+	/**
+	 * 解决与knife4j有兼容问题
+	 * @see https://github.com/xiaoymin/swagger-bootstrap-ui/issues/396
+	 * @see https://github.com/springfox/springfox/issues/3462
+	 * @return
+	 */
+	@Bean
+	public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+		return new BeanPostProcessor() {
+
+			@Override
+			public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+				if (bean instanceof WebMvcRequestHandlerProvider) {
+					customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+				}
+				return bean;
+			}
+
+			private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+				List<T> copy = mappings.stream()
+						.filter(mapping -> mapping.getPatternParser() == null)
+						.collect(Collectors.toList());
+				mappings.clear();
+				mappings.addAll(copy);
+			}
+
+			@SuppressWarnings("unchecked")
+			private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+				try {
+					Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+					field.setAccessible(true);
+					return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			}
+		};
+	}
 
 }
