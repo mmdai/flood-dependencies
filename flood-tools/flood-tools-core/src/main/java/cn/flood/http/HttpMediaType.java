82091fc400195eb37ce7cc3203343ef651726dd5
mmdai <daiming123.happy@163.com>
2022-04-01 17:01:28 +0800

    no message

--- flood-tools/flood-tools-core/src/main/java/cn/flood/http/HttpMediaType.java
+++ flood-tools/flood-tools-core/src/main/java/cn/flood/http/HttpMediaType.java
@@ -57,4 +57,15 @@ public class HttpMediaType {
     public static final String TEXT_PLAIN_VALUE = "text/plain";
 
     public static final String TEXT_XML_VALUE = "text/xml";
+
+    public static final String GET = "GET";
+
+    public static final String POST = "POST";
+    
+    public static final String PUT = "PUT";
+
+    public static final String DELETE = "DELETE";
+
+    public static final String PATCH = "PATCH";
+
 }
