260015f94973324574f345a7a3c72a94806536cc
mmdai <daiming123.happy@163.com>
2022-04-22 11:40:02 +0800

    no message

--- flood-spring-cloud-starter/flood-spring-cloud-starter-seata/src/main/java/cn/flood/cloud/seata/config/SeataDbConfiguration.java
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-seata/src/main/java/cn/flood/cloud/seata/config/SeataDbConfiguration.java
@@ -27,7 +27,7 @@ import java.sql.SQLException;
 @ConditionalOnBean(DataSource.class)
 @EnableAutoDataSourceProxy
 @Slf4j
-public class SeataConfiguration {
+public class SeataDbConfiguration {
 
     @Autowired
     public DataSource dataSource;
--- flood-spring-cloud-starter/flood-spring-cloud-starter-seata/src/main/java/cn/flood/cloud/seata/config/SeataFeginAutoConfiguration.java
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-seata/src/main/java/cn/flood/cloud/seata/config/SeataFeginAutoConfiguration.java
@@ -2,13 +2,8 @@ package cn.flood.cloud.seata.config;
 
 import cn.flood.cloud.config.FloodCloudAutoConfiguration;
 import cn.flood.cloud.seata.rest.SeataRestTemplateInterceptor;
-import com.alibaba.druid.pool.DruidDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 /**
  * @author mmdai
@@ -16,13 +11,13 @@ import org.springframework.context.annotation.Primary;
  * @date 2022/4/20 13:04
  */
 @AutoConfigureBefore({FloodCloudAutoConfiguration.class})
-public class SeataCondAutoConfiguration {
+public class SeataFeginAutoConfiguration {
 
-    @Primary
-    @Bean
-    public DataSourceProxy dataSource(@Qualifier("dataSource") DruidDataSource druidDataSource) {
-        return new DataSourceProxy(druidDataSource);
-    }
+//    @Primary
+//    @Bean
+//    public DataSourceProxy dataSource(DruidDataSource druidDataSource) {
+//        return new DataSourceProxy(druidDataSource);
+//    }
 
 
     @Bean
