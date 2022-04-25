9c3b2f298ea428ab5645095bd79b06f01ca70ab4
mmdai <daiming123.happy@163.com>
2022-04-25 13:44:53 +0800

    feat(module):  修改seata对tcc的支持

---
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-seata/src/main/java/cn/flood/cloud/seata/web/SeataHandlerInterceptor.java
@@ -0,0 +1,62 @@
+package cn.flood.cloud.seata.web;
+
+import io.seata.common.util.StringUtils;
+import io.seata.core.context.RootContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author mmdai
+ * @version 1.0
+ * @date 2022/4/25 10:29
+ */
+public class SeataHandlerInterceptor implements HandlerInterceptor {
+    private static final Logger log = LoggerFactory.getLogger(SeataHandlerInterceptor.class);
+
+    public SeataHandlerInterceptor() {
+    }
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String xid = RootContext.getXID();
+        String rpcXid = request.getHeader("TX_XID");
+        if (log.isDebugEnabled()) {
+            log.debug("xid in RootContext {} xid in RpcContext {}", xid, rpcXid);
+        }
+
+        if (StringUtils.isBlank(xid) && rpcXid != null) {
+            RootContext.bind(rpcXid);
+            if (log.isDebugEnabled()) {
+                log.debug("bind {} to RootContext", rpcXid);
+            }
+        }
+
+        return true;
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+        if (StringUtils.isNotBlank(RootContext.getXID())) {
+            String rpcXid = request.getHeader("TX_XID");
+            if (StringUtils.isEmpty(rpcXid)) {
+                return;
+            }
+
+            String unbindXid = RootContext.unbind();
+            if (log.isDebugEnabled()) {
+                log.debug("unbind {} from RootContext", unbindXid);
+            }
+
+            if (!rpcXid.equalsIgnoreCase(unbindXid)) {
+                log.warn("xid in change during RPC from {} to {}", rpcXid, unbindXid);
+                if (unbindXid != null) {
+                    RootContext.bind(unbindXid);
+                    log.warn("bind {} back to RootContext", unbindXid);
+                }
+            }
+        }
+
+    }
+}
---
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-seata/src/main/java/cn/flood/cloud/seata/web/SeataHandlerInterceptorConfiguration.java
@@ -0,0 +1,20 @@
+package cn.flood.cloud.seata.web;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author mmdai
+ * @version 1.0
+ * @date 2022/4/25 10:33
+ */
+@ConditionalOnWebApplication
+public class SeataHandlerInterceptorConfiguration implements WebMvcConfigurer {
+    public SeataHandlerInterceptorConfiguration() {
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SeataHandlerInterceptor()).addPathPatterns(new String[]{"/**"});
+    }
+}
