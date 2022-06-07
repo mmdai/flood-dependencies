d0ddc261938910d6b4b94db7f15e11f3c2de5ce8
mmdai <daiming123.happy@163.com>
2022-06-07 11:22:07 +0800

    fixed(module) ： cors跨域问题提到gateway，去掉spring的cors

--- flood-base/src/main/java/cn/flood/filter/CorsConfig.java
+++ flood-base/src/main/java/cn/flood/filter/CorsConfig.java
@@ -1,8 +1,6 @@
 package cn.flood.filter;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,19 +13,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
-    private final Long ACCESS_CONTROL_MAX_AGE = 3600L;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns(CorsConfiguration.ALL)
-                .allowCredentials(Boolean.TRUE)
-                .allowedMethods(CorsConfiguration.ALL)
-                .exposedHeaders("X-Access-Token", "Flood_Token","channel_id", "cache-control",
-                        "content-language", "client_id", "version", "tenant_id")
-                .maxAge(ACCESS_CONTROL_MAX_AGE);
-    }
+    /**
+     * gateway 网关已经添加 所以注释掉该代码
+     * @param registry
+     */
+//    private final Long ACCESS_CONTROL_MAX_AGE = 3600L;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns(CorsConfiguration.ALL)//允许跨域的主机地址,*表示所有都可以
+//                .allowCredentials(Boolean.TRUE)//是否允许携带cookie,true表示允许
+//                .allowedMethods(CorsConfiguration.ALL)//允许跨域的请求方法
+//                .exposedHeaders("X-Access-Token", "Flood_Token","channel_id", "cache-control",
+//                        "content-language", "client_id", "version", "tenant_id")//允许跨域的请求头
+//                .maxAge(ACCESS_CONTROL_MAX_AGE);//单位为秒, 重新预检验跨域的缓存时间,表示该时间内，不用重新检验跨域
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
