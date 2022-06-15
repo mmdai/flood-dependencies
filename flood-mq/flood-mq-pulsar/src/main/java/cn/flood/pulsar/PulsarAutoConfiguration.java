d26ece4699f701534cfc08bd36c9972c29e2dd55
mmdai <daiming123.happy@163.com>
2022-06-15 14:33:10 +0800

    no message

--- flood-mq/flood-mq-pulsar/src/main/java/cn/flood/pulsar/PulsarAutoConfiguration.java
+++ flood-mq/flood-mq-pulsar/src/main/java/cn/flood/pulsar/PulsarAutoConfiguration.java
@@ -8,6 +8,7 @@ import org.apache.pulsar.client.api.PulsarClientException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -16,14 +17,23 @@ import org.springframework.context.annotation.Bean;
  * @date 2022/6/15 12:12
  */
 @AutoConfiguration
+@EnableConfigurationProperties(PulsarProperties.class)
 @ConditionalOnExpression("'${pulsar.serviceUrl}'!=null")
 public class PulsarAutoConfiguration {
+
+    protected PulsarProperties pulsarProperties;
+
+    @Autowired
+    public void setPulsarProperties(PulsarProperties pulsarProperties) {
+        this.pulsarProperties = pulsarProperties;
+    }
+
 
     /**
      * create Pulsar client Bean
      */
     @Bean(value = "pulsarClient")
-    public PulsarClient pulsarClient(PulsarProperties pulsarProperties) throws PulsarClientException {
+    public PulsarClient pulsarClient() throws PulsarClientException {
         return PulsarClient.builder().serviceUrl(pulsarProperties.getServiceUrl()).build();
     }
 
@@ -31,7 +41,7 @@ public class PulsarAutoConfiguration {
      * create Pulsar Template Bean
      */
     @Bean(value = "pulsarTemplate")
-    public PulsarTemplate pulsarClient(@Autowired PulsarClient pulsarClient, PulsarProperties pulsarProperties) throws PulsarClientException {
+    public PulsarTemplate pulsarClient(@Autowired PulsarClient pulsarClient) throws PulsarClientException {
         return new PulsarTemplate(pulsarClient, pulsarProperties);
     }
 
@@ -39,7 +49,7 @@ public class PulsarAutoConfiguration {
      * create Pulsar Template Bean
      */
     @Bean(value = "pulsarConsumeClient")
-    public PulsarConsumeClient pulsarConsumeClient(@Autowired PulsarClient pulsarClient, PulsarProperties pulsarProperties) throws PulsarClientException {
+    public PulsarConsumeClient pulsarConsumeClient(@Autowired PulsarClient pulsarClient) throws PulsarClientException {
         return new PulsarConsumeClient(pulsarClient, pulsarProperties);
     }
 
