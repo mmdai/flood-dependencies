a84545da90dea634f71ea76e75b279f961fa37a6
benming110 <daiming123.happy@163.com>
2023-01-11 16:43:56 +0800

    no message

--- flood-spring-cloud-starter/flood-spring-cloud-starter-rule/src/main/java/cn/flood/cloud/rule/config/RuleConfiguration.java
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-rule/src/main/java/cn/flood/cloud/rule/config/RuleConfiguration.java
@@ -2,23 +2,15 @@ package cn.flood.cloud.rule.config;
 
 import cn.flood.cloud.rule.service.RuleCacheService;
 import cn.flood.cloud.rule.service.impl.RuleCacheServiceImpl;
-import cn.flood.db.redis.config.lettuce.LettuceConnectionConfiguration;
 import cn.flood.db.redis.service.RedisService;
-import cn.flood.db.redis.util.ApplicationContextUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 /**
  * 规则配置
  * @author pangu
  */
 @AutoConfiguration
-@Import({
-        RedisService.class
-})
 public class RuleConfiguration {
 
     @Bean
