f1a857bd6bdb2030f9a4e64539c1d66301ca6028
benming110 <daiming123.happy@163.com>
2023-06-28 18:02:47 +0800

    no message

--- flood-spring-cloud-starter/flood-spring-cloud-starter-gateway/src/main/java/cn/flood/cloud/gateway/config/CustomErrorConfig.java
+++ flood-spring-cloud-starter/flood-spring-cloud-starter-gateway/src/main/java/cn/flood/cloud/gateway/config/CustomErrorConfig.java
@@ -1,10 +1,12 @@
 package cn.flood.cloud.gateway.config;
 
+import cn.flood.cloud.gateway.handler.ExceptionHandlerAdvice;
 import cn.flood.cloud.gateway.handler.JsonErrorExceptionHandler;
 import cn.flood.cloud.gateway.props.ResourceProperties;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -18,7 +20,6 @@ import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -29,7 +30,7 @@ import org.springframework.web.reactive.result.view.ViewResolver;
  *
  * @author mmdai
  */
-@Configuration
+@AutoConfiguration
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
 @ConditionalOnClass(WebFluxConfigurer.class)
 @AutoConfigureBefore(WebFluxAutoConfiguration.class)
@@ -60,15 +61,23 @@ public class CustomErrorConfig {
   }
 
   @Bean
+  @ConditionalOnMissingBean(value = ExceptionHandlerAdvice.class)
+  public ExceptionHandlerAdvice exceptionHandlerAdvice() {
+    return new ExceptionHandlerAdvice();
+  }
+
+  @Bean
   @ConditionalOnMissingBean(value = ErrorWebExceptionHandler.class,
       search = SearchStrategy.CURRENT)
   @Order(-1)
-  public ErrorWebExceptionHandler errorWebExceptionHandler(ErrorAttributes errorAttributes) {
+  public ErrorWebExceptionHandler errorWebExceptionHandler(ErrorAttributes errorAttributes,
+                                                           ExceptionHandlerAdvice exceptionHandlerAdvice) {
     JsonErrorExceptionHandler exceptionHandler = new JsonErrorExceptionHandler(
         errorAttributes,
         resourceProperties,
         this.serverProperties.getError(),
-        applicationContext);
+        applicationContext,
+            exceptionHandlerAdvice);
     exceptionHandler.setViewResolvers(this.viewResolvers);
     exceptionHandler.setMessageWriters(this.serverCodecConfigurer.getWriters());
     exceptionHandler.setMessageReaders(this.serverCodecConfigurer.getReaders());
