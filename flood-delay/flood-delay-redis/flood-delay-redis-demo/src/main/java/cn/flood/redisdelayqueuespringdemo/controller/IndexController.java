b920d9881a563e9c2418f62874c79b08af1b918b
benming110 <daiming123.happy@163.com>
2024-07-16 14:04:45 +0800

    no message

--- flood-delay/flood-delay-redis/flood-delay-redis-demo/src/main/java/cn/flood/redisdelayqueuespringdemo/controller/IndexController.java
+++ flood-delay/flood-delay-redis/flood-delay-redis-demo/src/main/java/cn/flood/redisdelayqueuespringdemo/controller/IndexController.java
@@ -1,7 +1,6 @@
 package cn.flood.redisdelayqueuespringdemo.controller;
 
 import cn.flood.base.core.json.JsonUtils;
-import cn.flood.db.redis.service.RedisService;
 import cn.flood.delay.redis.RDQueueTemplate;
 import cn.flood.delay.redis.core.Message;
 import cn.flood.delay.redis.exception.RDQException;
@@ -24,8 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-  @Autowired
-  RedisService redisService;
+//  @Autowired
+//  RedisService redisService;
   @Autowired
   RedisTemplate redisTemplate;
 
@@ -83,45 +82,45 @@ public class IndexController {
   }
 
 
-  @GetMapping("/save")
-  public void save() {
-    User user = new User();
-    user.setId(1234);
-//        user.setDateTime(LocalDateTime.now());
-    user.setName("哈哈");
-    user.setContent("我猜你是ABC");
-    List<User> list = new ArrayList<>();
-    list.add(user);
-    redisService.set("test", list, 30L, TimeUnit.MINUTES);
-  }
-
-  @GetMapping("/get")
-  public void get() {
-    List<User> user = redisService.get("test");
-    System.out.println(JsonUtils.toJSONString(user));
-  }
-
-  @GetMapping("/save1")
-  public void save1() {
-    redisService.addZSet("test1", "22", "44", "33", "22");
-  }
-
-  @GetMapping("/get1")
-  public void get1() {
-    Map<Double, Object> value = redisService.popMaxByScoreZSet("test1", 2);
-    System.out.println(value);
-  }
-
-  @GetMapping("/save2")
-  public void save2() {
-    redisService.lpushAll("test2", "22", "44", "33", "22");
-  }
-
-  @GetMapping("/get2")
-  public void get2() {
-    String value = redisService.lpop("test2");
-    System.out.println(value);
-  }
+//  @GetMapping("/save")
+//  public void save() {
+//    User user = new User();
+//    user.setId(1234);
+////        user.setDateTime(LocalDateTime.now());
+//    user.setName("哈哈");
+//    user.setContent("我猜你是ABC");
+//    List<User> list = new ArrayList<>();
+//    list.add(user);
+//    redisService.set("test", list, 30L, TimeUnit.MINUTES);
+//  }
+//
+//  @GetMapping("/get")
+//  public void get() {
+//    List<User> user = redisService.get("test");
+//    System.out.println(JsonUtils.toJSONString(user));
+//  }
+//
+//  @GetMapping("/save1")
+//  public void save1() {
+//    redisService.addZSet("test1", "22", "44", "33", "22");
+//  }
+//
+//  @GetMapping("/get1")
+//  public void get1() {
+//    Map<Double, Object> value = redisService.popMaxByScoreZSet("test1", 2);
+//    System.out.println(value);
+//  }
+//
+//  @GetMapping("/save2")
+//  public void save2() {
+//    redisService.lpushAll("test2", "22", "44", "33", "22");
+//  }
+//
+//  @GetMapping("/get2")
+//  public void get2() {
+//    String value = redisService.lpop("test2");
+//    System.out.println(value);
+//  }
 
   @GetMapping("/save3")
   public void save3() {
